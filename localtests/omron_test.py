b46f87f75be7ff33b3386c37a0e839281cfb10cd
Гранин Дмитрий Борисович <GraninDB@foamline.com>
2025-03-12 15:09:18 +0300

    Small update

--- localtests/omron_test.py
+++ localtests/omron_test.py
@@ -8,18 +8,20 @@ To see the Tag operations succeed, fire up:
 import os
 import sys
 import logging
+
 sys.path.insert(0, '..')
 sys.path.insert(0, '../..')
 
 import configparser
+from cpppo.server.enip import address, client
+
 try:
     import cpppo
 except ImportError:
     # Allow import of 'cpppo' when executing within 'cpppo' package directory
-    sys.path.insert( 0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     import cpppo
 
-from cpppo.server.enip import address, client
 
 use_omron = True
 dialect = None
@@ -33,26 +35,28 @@ locatest_ini.read('localtests\\localtest.ini')
 omron_ip = locatest_ini['omron']['ip']
 
 if __name__ == "__main__":
-    logging.basicConfig( **cpppo.log_cfg )
-    #logging.getLogger().setLevel(logging.DEBUG)
-    host			= omron_ip	# Controller IP address
-    port			= address[1]	# default is port 44818
-    depth			= 1		# Allow 1 transaction in-flight
-    multiple		= 0		# Don't use Multiple Service Packet
-    fragment		= False		# Don't force Read/Write Tag Fragmented
-    timeout			= 1.0		# Any PLC I/O fails if it takes > 1s
-    printing		= True		# Print a summary of I/O
-    #tags			= ["Blocks[1,1,1].ID", "Test_input_bool", "Test_input_string"]
-    #tags			= ["Blocks[3,1,1].Foam_type"]
-    tags			= ["Blocks[3,1,1].Racks_IN.M_Date"]
-    #tags			= ["Test_input_bool"]
+    logging.basicConfig(**cpppo.log_cfg)
+#   logging.getLogger().setLevel(logging.DEBUG)
+    host = omron_ip     # Controller IP address
+    port = address[1]   # default is port 44818
+    depth = 1		    # Allow 1 transaction in-flight
+    multiple = 0		# Don't use Multiple Service Packet
+    fragment = False    # Don't force Read/Write Tag Fragmented
+    timeout = 1.0		# Any PLC I/O fails if it takes > 1s
+    printing = True		# Print a summary of I/O
+#   tags = ["Blocks[1,1,1].ID", "Test_input_bool", "Test_input_string"]
+#   tags = ["Blocks[1,1,1].ID"]
+    tags = ["Blocks[1,1,1].Length_Stamp_Start"]
+#   tags = ["Blocks[3,1,1].Foam_type"]
+#   tags = ["Blocks[3,1,1].Racks_IN.M_Date"]
+#   tags = ["Test_input_bool"]
 
-    with client.connector( host=host, port=port, timeout=timeout, dialect=dialect ) as connection:
-        operations		= client.parse_operations( tags )
-        failures,transactions	= connection.process(
+    with client.connector(host=host, port=port, timeout=timeout, dialect=dialect) as connection:
+        operations = client.parse_operations(tags)
+        failures, transactions = connection.process(
             operations=operations, depth=depth, multiple=multiple,
-            fragment=fragment, printing=printing, timeout=timeout )
+            fragment=fragment, printing=printing, timeout=timeout)
 
     print(transactions)
 
-    sys.exit( 1 if failures else 0 )
+    sys.exit(1 if failures else 0)
